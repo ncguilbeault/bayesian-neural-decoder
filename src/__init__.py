9fe2952785897b4802e76998aaa0b18094ed8474
ncguilbeault <ncguilbeault@gmail.com>
2024-11-18 11:30:32 +0000

    Added init

---
+++ src/__init__.py
@@ -0,0 +1,4 @@
+from .decoder import SortedSpikeDecoder, ClusterlessSpikeDecoder
+from .data_loader import DataLoader
+from .data_iterator import DataIterator
+from .likelihood import LIKELIHOOD_FUNCTION
